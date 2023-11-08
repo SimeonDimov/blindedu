d1037377a4b3d8e9866c9294f3de045b94e14ee9
Simeon <simeondimow@gmail.com>
2023-11-08 11:06:19 +0100

    updated full functionality and added readme

--- README.md
+++ README.md
@@ -1,16 +1,60 @@
-# blindedu
+# ESP32 BLE LED Control
 
-A new Flutter project.
+This repository contains a Flutter application that communicates with an ESP32 module over BLE to control an onboard LED. The application scans for BLE devices, connects to the ESP32, and sends commands to toggle the LED state.
 
-## Getting Started
+## Flutter App Setup
 
-This project is a starting point for a Flutter application.
+### Dependencies
 
-A few resources to get you started if this is your first Flutter project:
+- `flutter_reactive_ble`: Handles BLE operations
+- `permission_handler`: Manages permission requests
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+### Running the App
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+1. Clone the repository.
+2. Navigate to the project directory.
+3. Run `flutter pub get` to install dependencies.
+4. Connect a device or use an emulator.
+5. Execute `flutter run` to start the app.
+
+### Key Features
+
+- Scans for devices advertising the specified BLE service.
+- Connects to the ESP32 device and manages connection status.
+- Sends commands to turn the LED on or off.
+- Receives and displays feedback from the ESP32 regarding LED state.
+
+## ESP32 Firmware Setup
+
+### Key Components
+
+- `BLEService`: BLE service with a unique UUID. (12345678-1234-5678-1234-56789abcdef0) Default setup, already preset in the code.
+- `BLEStringCharacteristic`: Characteristic to receive commands. ("87654321-4321-6789-4321-6789abcdef01", BLEWrite | BLERead | BLENotify, 20) Default setup, already preset in the code.
+- `LED_BUILTIN`: Built-in LED for feedback.
+
+### Uploading the Firmware
+
+1. Open Arduino IDE.
+2. Install the `ArduinoBLE` library via the Library Manager.
+3. Load the `esp32.ino` sketch file.
+4. Configure the correct port and board settings for your ESP32 module.
+5. Upload the sketch to the ESP32.
+
+Upload your ESP32 code using the Arduino IDE, ensuring you've selected the correct board and port for your ESP32 module.
+
+### Functionality
+
+- Advertises a BLE service that the Flutter app can discover.
+- Listens for incoming commands to control the LED state.
+- Sends back the LED status as a BLE notification.
+
+## iOS Specific Setup
+
+- Install Xcode on your Mac.
+- Configure an Apple Developer account for app signing.
+- Set up the correct provisioning profiles and certificates.
+- Update `Info.plist` with necessary permissions descriptions.
+
+## Contributions
+
+This project is open for contributions. Please follow the standard fork-and-pull-request workflow. Make sure your code adheres to the existing coding standards.
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'dart:async';
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -8,7 +9,7 @@ import 'package:permission_handler/permission_handler.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -19,29 +20,38 @@ class MyApp extends StatelessWidget {
 }
 
 class BLEControlPage extends StatefulWidget {
-  const BLEControlPage({Key? key}) : super(key: key);
+  const BLEControlPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _BLEControlPageState createState() => _BLEControlPageState();
 }
 
 class _BLEControlPageState extends State<BLEControlPage> {
+  bool _awaitingResponse = false;
+
   final _ble = FlutterReactiveBle();
   final _serviceUuid = Uuid.parse("12345678-1234-5678-1234-56789abcdef0");
   final _characteristicUuid =
       Uuid.parse("87654321-4321-6789-4321-6789abcdef01");
   StreamSubscription<ConnectionStateUpdate>? _connectionSubscription;
   StreamSubscription<DiscoveredDevice>? _scanStreamSubscription;
+  StreamSubscription<List<int>>? _feedbackSubscription;
+
   final List<DiscoveredDevice> _foundBleDevices = [];
   DiscoveredDevice? _connectedDevice;
   QualifiedCharacteristic? _qualifiedCharacteristic;
   bool _isConnected = false;
-
+  bool _isScanning = false;
+  String _statusMessage = ''; // Add this line to define the variable
+  Color _connectionStatusColor = Colors.red;
+  @override
   @override
   void initState() {
     super.initState();
-    _requestPermissions();
+    _requestPermissions().then((_) {
+      // Start scanning for devices as soon as the app starts and permissions are granted
+      startScan();
+    });
   }
 
   Future<void> _requestPermissions() async {
@@ -76,80 +86,163 @@ class _BLEControlPageState extends State<BLEControlPage> {
   }
 
   void startScan() {
+    if (_isScanning) return; // Prevent multiple scans at the same time
+    _foundBleDevices.clear(); // Clear the list each time you start scanning
+    setState(() {
+      _statusMessage = 'Scanning for devices...';
+      _isScanning = true;
+    });
+
+    _scanStreamSubscription?.cancel(); // Cancel any existing scan
     _scanStreamSubscription = _ble.scanForDevices(
       withServices: [_serviceUuid],
       scanMode: ScanMode.lowLatency,
-    ).listen((device) {
-      if (!_foundBleDevices.any((d) => d.id == device.id)) {
+    ).listen(
+      (device) {
+        if (!_foundBleDevices.any((d) => d.id == device.id)) {
+          setState(() {
+            _foundBleDevices.add(device);
+          });
+        }
+      },
+      onError: (Object error) {
         setState(() {
-          _foundBleDevices.add(device);
+          _statusMessage = 'Error occurred while scanning. Please try again.';
+          _isScanning = false;
         });
-      }
-    }, onError: (Object error) {
-      if (kDebugMode) {
-        print('Scan Error: $error');
-      }
-      setState(() {});
+      },
+    );
+  }
+
+  void stopScan() {
+    _scanStreamSubscription?.cancel();
+    setState(() {
+      _isScanning = false;
+      _statusMessage = 'Scan completed';
+      // Optionally clear the list if you want to start fresh next time
+      // _foundBleDevices.clear();
     });
   }
 
   Future<void> _connectToDevice(DiscoveredDevice device) async {
+    _connectionSubscription?.cancel(); // Cancel previous subscription if any
     _connectionSubscription = _ble
         .connectToDevice(
       id: device.id,
       servicesWithCharacteristicsToDiscover: {
-        _serviceUuid: [_characteristicUuid]
+        _serviceUuid: [_characteristicUuid],
       },
       connectionTimeout: const Duration(seconds: 5),
     )
-        .listen((connectionState) {
-      if (connectionState.connectionState == DeviceConnectionState.connected) {
-        setState(() {
-          _isConnected = true;
-          _connectedDevice =
-              device; // Store the device object passed to the method
-        });
-        // Retrieve and store the QualifiedCharacteristic
-        _qualifiedCharacteristic = QualifiedCharacteristic(
-          serviceId: _serviceUuid,
-          characteristicId: _characteristicUuid,
-          deviceId: device.id,
-        );
-      } else if (connectionState.connectionState ==
-          DeviceConnectionState.disconnected) {
+        .listen(
+      (connectionState) {
+        if (connectionState.connectionState ==
+            DeviceConnectionState.connected) {
+          _qualifiedCharacteristic = QualifiedCharacteristic(
+            serviceId: _serviceUuid,
+            characteristicId: _characteristicUuid,
+            deviceId: device.id,
+          );
+
+          // Call _listenToFeedback here to start listening for feedback
+          _listenToFeedback();
+
+          setState(() {
+            _isConnected = true;
+            _connectedDevice = device;
+            _statusMessage = 'Connected to ${device.name}';
+            _connectionStatusColor = Colors.green;
+          });
+        } else if (connectionState.connectionState ==
+            DeviceConnectionState.disconnected) {
+          setState(() {
+            _isConnected = false;
+            _connectedDevice = null;
+            _qualifiedCharacteristic = null;
+            _statusMessage = 'Disconnected';
+            _connectionStatusColor = Colors.red;
+          });
+        }
+      },
+      onError: (dynamic error) {
         setState(() {
           _isConnected = false;
           _connectedDevice = null;
           _qualifiedCharacteristic = null;
+          _statusMessage = 'Connection error: ${error.toString()}';
+          _connectionStatusColor = Colors.red;
         });
-      }
-    }, onError: (dynamic error) {
-      setState(() {});
-    });
+      },
+    );
   }
 
-  void _disconnectFromDevice() {
-    _connectionSubscription?.cancel();
-    setState(() {
-      _isConnected = false;
-      _connectedDevice = null;
-      _qualifiedCharacteristic = null;
+// Add the _listenToFeedback function
+  void _listenToFeedback() {
+    if (_qualifiedCharacteristic == null) {
+      setState(() {
+        _statusMessage = 'Device not connected for feedback.';
+        _connectionStatusColor = Colors.red;
+      });
+      return;
+    }
+
+    _feedbackSubscription?.cancel(); // Cancel any existing subscription
+    _feedbackSubscription = _ble
+        .subscribeToCharacteristic(_qualifiedCharacteristic!)
+        .listen((data) {
+      if (!_awaitingResponse) {
+        return; // Ignore the data if we are not expecting a response
+      }
+
+      final responseValue = String.fromCharCodes(data);
+      if (responseValue.contains('LED ON') ||
+          responseValue.contains('LED OFF')) {
+        setState(() {
+          _statusMessage = 'ESP32 says: $responseValue';
+          _connectionStatusColor = Colors.green;
+        });
+        _awaitingResponse = false; // Reset the flag after getting a response
+      }
+    }, onError: (dynamic error) {
+      setState(() {
+        _statusMessage = 'Feedback error: $error';
+        _connectionStatusColor = Colors.red;
+      });
+      _awaitingResponse = false; // Reset the flag if there's an error
     });
   }
 
   Future<void> _sendCommand(String command) async {
-    if (_qualifiedCharacteristic == null) return;
+    if (_qualifiedCharacteristic == null) {
+      setState(() {
+        _statusMessage = 'Device not connected.';
+        _connectionStatusColor = Colors.red;
+      });
+      return;
+    }
 
-    await _ble.writeCharacteristicWithResponse(
-      _qualifiedCharacteristic!,
-      value: command.codeUnits,
-    );
+    try {
+      _awaitingResponse = true; // We are now awaiting a response
+
+      await _ble.writeCharacteristicWithResponse(
+        _qualifiedCharacteristic!,
+        value: command.codeUnits,
+      );
+
+      // No need to subscribe here again if we're already listening for feedback elsewhere
+    } catch (e) {
+      setState(() {
+        _statusMessage = 'Error sending command: $e';
+        _connectionStatusColor = Colors.red;
+      });
+    }
   }
 
   @override
   void dispose() {
     _scanStreamSubscription?.cancel();
     _connectionSubscription?.cancel();
+    _feedbackSubscription?.cancel(); // Add this line
     super.dispose();
   }
 
@@ -161,18 +254,46 @@ class _BLEControlPageState extends State<BLEControlPage> {
         actions: [
           if (_isConnected)
             IconButton(
-              icon: const Icon(Icons.cancel),
-              onPressed: _disconnectFromDevice,
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                setState(() {
+                  _isConnected = false;
+                  _connectedDevice = null;
+                  _qualifiedCharacteristic = null;
+                });
+              },
             ),
         ],
       ),
-      body: _isConnected
-          ? _buildConnectedDeviceInterface()
-          : _buildScanningInterface(),
-      floatingActionButton: _isConnected
-          ? null
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Centered status bar
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              color: _connectionStatusColor,
+              child: Center(
+                // Center widget added here
+                child: Text(
+                  _statusMessage,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Expanded(
+              child: _isConnected
+                  ? _buildConnectedDeviceInterface()
+                  : _buildScanningInterface(),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: _isScanning || _foundBleDevices.isNotEmpty
+          ? null // Hide the button if scanning or devices are found
           : FloatingActionButton(
               onPressed: startScan,
+              backgroundColor: Theme.of(context).primaryColor,
               child: const Icon(Icons.bluetooth_searching),
             ),
     );
@@ -196,6 +317,9 @@ class _BLEControlPageState extends State<BLEControlPage> {
   }
 
   Widget _buildScanningInterface() {
+    if (_foundBleDevices.isEmpty) {
+      return Center(child: Text(_statusMessage)); // Display the status message
+    }
     return ListView.builder(
       itemCount: _foundBleDevices.length,
       itemBuilder: (context, index) {
